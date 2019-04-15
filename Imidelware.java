6c6631222dee2af3662d671e168b040c0d0abd44
Amenallah88 <47630380+Amenallah88@users.noreply.github.com>
2019-04-15 10:01:27 +0200

    Update Imidelware.java

--- Imidelware.java
+++ Imidelware.java
@@ -1,6 +1,6 @@
 public class Imidelware {
 	
-	public static void main(Sting[] args){
+	public static void main(String[] args){
 		
 		System.out.println("Bonjour Mr Amenallah");
 		System.out.println("Bon courage pour la formation");
